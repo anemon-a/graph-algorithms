f15ac08834bac50bdeb007c19940259fa5eb466e
anemone <annamatskevich866@gmail.com>
2024-10-02 23:57:08 +0700

    feature: add GetShortestPathBetweenVertices

--- src/s21_graph_algorithms.cc
+++ src/s21_graph_algorithms.cc
@@ -1,27 +1,5 @@
 #include "s21_graph_algorithms.h"
 
-std::vector<int> GraphAlgorithms::BreadthFirstSearch(IGraph &graph,
-                                                     int start_vertex) {
-  std::queue<int> queue;
-  std::vector<int> result;
-  std::unordered_set<int> visited;
-  queue.push(start_vertex);
-  while (!queue.empty()) {
-    int v_from = queue.front();
-    if (!visited.count(v_from)) {
-      for (const int &v_to : graph.GetNeighbourVertices(v_from)) {
-        if (!visited.count(v_to)) {
-          queue.push(v_to);
-        }
-      }
-      visited.insert(v_from);
-      result.push_back(v_from);
-    }
-    queue.pop();
-  }
-  return result;
-}
-
 std::vector<int> GraphAlgorithms::DepthFirstSearch(IGraph &graph,
                                                    int start_vertex) {
   std::stack<int> stack;
@@ -44,4 +22,65 @@ std::vector<int> GraphAlgorithms::DepthFirstSearch(IGraph &graph,
   }
 
   return result;
+}
+
+std::vector<int> GraphAlgorithms::BreadthFirstSearch(IGraph &graph,
+                                                     int start_vertex) {
+  std::queue<int> queue;
+  std::vector<int> result;
+  std::unordered_set<int> visited;
+  queue.push(start_vertex);
+  while (!queue.empty()) {
+    int v_from = queue.front();
+    if (!visited.count(v_from)) {
+      for (const int &v_to : graph.GetNeighbourVertices(v_from)) {
+        if (!visited.count(v_to)) {
+          queue.push(v_to);
+        }
+      }
+      visited.insert(v_from);
+      result.push_back(v_from);
+    }
+    queue.pop();
+  }
+  return result;
+}
+
+// int FindLowestDistanceVertex(const std::map<int, int> &distance,
+//                              const std::unordered_set<int> &unvisited) {}
+
+int GraphAlgorithms::GetShortestPathBetweenVertices(IGraph &graph, int vertex1,
+                                                    int vertex2) {
+  std::map<int, int> vertex_distance;
+  std::unordered_set<int> unvisited;
+  for (auto vertex : graph.GetAllVertices()) {
+    vertex_distance[vertex] = std::numeric_limits<int>::max();
+    unvisited.insert(vertex);
+  }
+  vertex_distance[vertex1] = 0;
+  while (!unvisited.empty()) {
+    int min_distance = std::numeric_limits<int>::max();
+    int min_distance_vertex = -1;
+
+    for (const auto &[vertex, distance] : vertex_distance) {
+      if (distance < min_distance && unvisited.count(vertex)) {
+        min_distance = distance;
+        min_distance_vertex = vertex;
+      }
+    }
+
+    for (const auto &neighbour_vertex :
+         graph.GetNeighbourVertices(min_distance_vertex)) {
+      int new_distance = min_distance + graph.GetEdgeWeight(min_distance_vertex,
+                                                            neighbour_vertex);
+
+      if (new_distance < vertex_distance[neighbour_vertex]) {
+        // std::cout << neighbour_vertex << ' ' << new_distance << '\n';
+        vertex_distance[neighbour_vertex] = new_distance;
+      }
+    }
+    unvisited.erase(min_distance_vertex);
+  }
+  // std::cout << vertex_distance[vertex2];
+  return vertex_distance[vertex2];
 }
--- src/s21_graph_algorithms.h
+++ src/s21_graph_algorithms.h
@@ -1,6 +1,8 @@
 #ifndef S21_GRAPH_ALGORITHMS_H_
 #define S21_GRAPH_ALGORITHMS_H_
 
+#include <limits>
+#include <map>
 #include <queue>
 #include <stack>
 #include <unordered_set>
@@ -13,6 +15,7 @@ class GraphAlgorithms {
   std::vector<int> DepthFirstSearch(IGraph &graph, int start_vertex);  // stack
   std::vector<int> BreadthFirstSearch(IGraph &graph,
                                       int start_vertex);  // queue
+  int GetShortestPathBetweenVertices(IGraph &graph, int vertex1, int vertex2);
 };
 
 #endif  // S21_GRAPH_ALGORITHMS_H_
